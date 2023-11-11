1cb69418bd1fcb7944fc7abb3ff0c6cf002fe38d
n-katou <102382177+n-katou@users.noreply.github.com>
2023-11-11 23:54:42 +0900

    logout機能実装

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate,login,logout
 from .models import BoardModel
 from django.contrib.auth.decorators import login_required
 
@@ -44,3 +44,7 @@ def loginfunc(request):
 def listfunc(request):
     object_list = BoardModel.objects.all()
     return render(request, 'list.html', {'object_list': object_list})
+
+def logoutfunc(request):
+    logout(request)
+    return redirect('login')
